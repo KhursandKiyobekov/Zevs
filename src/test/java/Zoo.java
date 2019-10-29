5144935984640ff4d160ddcfd4564c63da980dc2
khursandkiyobekov <hursandkiebekov@gmail.com>
2019-10-28 20:59:52 -0400

    Day5-Add kind and zoo classes

--- src/test/java/Zoo.java
+++ src/test/java/Zoo.java
@@ -1,9 +1,9 @@
-public class zoo {
+public class Zoo {
     public static void main(String[] args) {
-        kind mykind = new kind();
-        mykind.setName = ("myau");
-                mykind.setAge = (3);
-                mykind.setWeight = (5.5);
+        Kind mykind = new Kind();
+        mykind.setName("myau");
+        mykind.setAge(3);
+        mykind.setWeight(5.5);
         System.out.println("Name:\t" + mykind());
         System.out.println("Age:\t" + mykind());
         System.out.println("Weight:\t" + mykind());
