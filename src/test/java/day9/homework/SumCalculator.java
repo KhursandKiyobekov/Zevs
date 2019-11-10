02cd5c2927aee317c8a0c2f89e97a19483c50133
khursandkiyobekov <hursandkiebekov@gmail.com>
2019-11-10 16:43:48 -0500

    Day5-Addtoomuch))

---
+++ src/test/java/day9/homework/SumCalculator.java
@@ -0,0 +1,44 @@
+package day9.homework;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SumCalculator {
+    public int calculateSum(int maxValue) {
+        int sum = 0;
+        for (int i = 0; i<=maxValue; i++ ) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+    @Test
+    public void testSumOfZero () {
+        assertEquals(0, new SumCalculator().calculateSum(0));
+    }
+
+    @Test
+    public void testSumOfOne () {
+        assertEquals(1, new SumCalculator().calculateSum(1));
+    }
+
+    @Test
+    public void testSumOfTwo () {
+        assertEquals(3, new SumCalculator().calculateSum(2));
+    }
+
+    @Test
+    public void testSumOfThree () {
+        assertEquals(6, new SumCalculator().calculateSum(3));
+    }
+
+    @Test
+    public void testSumOfFour () {
+        assertEquals(10, new SumCalculator().calculateSum(4));
+    }
+
+    @Test
+    public void testSumOfOneTwenty () {
+        assertEquals(120, new SumCalculator().calculateSum(15));
+    }
+}
